fda05a85c4bd55ba270dfcb9f4e4d0380c4a3e5f
kourachanis <65910089+kourachanis@users.noreply.github.com>
2020-05-29 14:50:23 +0300

    Update Database_Connector.java

--- Database_Connector.java
+++ Database_Connector.java
@@ -1,12 +1,23 @@
 package com.database;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+ import java.sql.DriverManager;
+ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement; 
 
 public class Database_Connector {
 	public static void main(String args[]) throws ClassNotFoundException { 
+		
+	       Database_Connector d = new Database_connector();
+	       d.connector();
+		
+	}
+	
+	public static String name;
+	public static String pass;
+	
+	public static void connector()  {
+		
 		String driver = "com.mysql.jdbc.Driver";
 		String dbURL = "jdbc:mysql://esocial.gr/fixitup"; 
 		String username ="fixitup"; 
@@ -15,18 +26,25 @@ public class Database_Connector {
 		Statement stmt = null; 
 		ResultSet rs = null; 
 		String query ="select `uname` from Users"; 
-		Class.forName(driver);
-
+		try {
+		         Class.forName(driver);
+		} catch  ClassNotFoundException e) {
+			 // TODO Auto-generated catch block
+			 e.printStackTrace();
+		}
+		
 		try { 
-			dbCon = DriverManager.getConnection(dbURL, username, password); //getting database connection to MySQL server
-			 stmt = dbCon.prepareStatement(query); //getting PreparedStatment to execute query
-			 rs = stmt.executeQuery(query); //Resultset returned by query
+			//getting database connection to MySQL server
+			dbCon = DriverManager.getConnection(dbURL, username, password); //getting PreparedStatment to execute query
+			stmt = dbCon.prepareStatement(query); //Resultset returned by query
+			rs = stmt.executeQuery(query); 
 			rs.next();
-			String name = rs.getString("uname");
+			name = rs.getString("uname");
 			System.out.print(dbCon);
 			System.out.print(name);
 			while(rs.next()){ int count = rs.getInt(1); 
-      System.out.println("count of stock : " + count); } }
+      System.out.println("count of stock : " + count); } 
+                    }
 		catch (SQLException ex) {  } 
 		finally{ 
 			//close connection ,stmt and resultset here 
