208e3eff1eeaaecae0ea03ff85cc8739a33ddeef
mariannazch <65726276+mariannazch@users.noreply.github.com>
2020-06-02 15:06:54 +0300

    Create Log_out.java

---
+++ Log_out.java
@@ -0,0 +1,23 @@
+package com.fixitup.MainApp;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Panel_Logout extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_Logout() {
+		setBounds(186,11,662,546);
+		setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(158, 149, 89, 23);
+		add(btnNewButton);
+		setVisible(true);
+		
+	}
+}
