433f6c114f711491582ed77fcbd94d7e481c786c
kourachanis <65910089+kourachanis@users.noreply.github.com>
2020-06-02 18:54:36 +0300

    Create Send_Message.java

---
+++ Send_Message.java
@@ -0,0 +1,14 @@
+package com.fixitup.MainApp;
+
+import javax.swing.JPanel;
+
+public class Panel_SendMessage extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_SendMessage() {
+		setBounds(186,11,662,546);
+	}
+
+}
