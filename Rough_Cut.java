3c518c5a2e99c8f3f61d636b4188161f0ce423c2
mariannazch <65726276+mariannazch@users.noreply.github.com>
2020-06-02 16:38:03 +0300

    Create Rough_Cut.java

---
+++ Rough_Cut.java
@@ -0,0 +1,14 @@
+package com.fixitup.MainApp;
+
+import javax.swing.JPanel;
+
+public class Panel_RoughCut extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_RoughCut() {
+		setBounds(186,11,662,546);
+	}
+
+}
