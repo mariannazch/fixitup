fc98cedf3d488ddcf722026d6874007f1d34f28c
kotsosliop <64268347+kotsosliop@users.noreply.github.com>
2020-06-02 19:05:23 +0300

    Create backitup.java

---
+++ backitup.java
@@ -0,0 +1,15 @@
+package com.fixitup.MainApp;
+
+import javax.swing.JPanel;
+
+public class Panel_Backitup extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_Backitup() {
+		setBounds(186,11,662,546);
+
+	}
+
+}
