1525ead3249b0cab4c7d50fd0f0e0f4a57da1ee8
kwnnosoik <65904207+kwnnosoik@users.noreply.github.com>
2020-06-03 22:52:09 +0300

    Create Panel_home.java

---
+++ Panel_home.java
@@ -0,0 +1,18 @@
+package com.fixitup.MainApp;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class Panel_home extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_home() {
+		setBounds(186,11,662,546);
+		
+		JButton btnNewButton = new JButton("htgstjrstjrdjrsd");
+		add(btnNewButton);
+	}
+
+}
