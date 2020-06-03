a9bb9f8ee51885793655fe744fba7b6381497f68
kwnnosoik <65904207+kwnnosoik@users.noreply.github.com>
2020-06-03 22:54:28 +0300

    Create Panel_ConsumerReq.java

---
+++ Panel_ConsumerReq.java
@@ -0,0 +1,14 @@
+package com.fixitup.MainApp;
+
+import javax.swing.JPanel;
+
+public class Panel_ConsumerReq extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_ConsumerReq() {
+		setBounds(186,11,662,546);
+	}
+
+}
