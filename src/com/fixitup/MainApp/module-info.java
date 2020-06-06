bfcef35d22c44edee3e4b3f2f6e8b1cf0fe58604
kwnnosoik <65904207+kwnnosoik@users.noreply.github.com>
2020-06-06 15:36:22 +0300

    Update and rename info.java to src/com/fixitup/MainApp/module-info.java

--- src/com/fixitup/MainApp/module-info.java
+++ src/com/fixitup/MainApp/module-info.java
@@ -1,14 +1,11 @@
 package com.fixitup.MainApp;
-/**
- * 
- */
-/**
- *
- */
+
 module FixitUp {
 	exports com.fixitup.MainApp;
 	exports com.database;
-
+	exports com.testunits;
 	requires java.desktop;
 	requires java.sql;
+	requires junit;
+	requires org.junit.jupiter.api;
 }
