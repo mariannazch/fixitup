9f37a320c3af0bdc6b4a22eb271e93c5f9b640b7
mariannazch <65726276+mariannazch@users.noreply.github.com>
2020-06-06 15:00:43 +0300

    Create Panel_FinalDelivery.java

---
+++ Panel_FinalDelivery.java
@@ -0,0 +1,14 @@
+package com.fixitup.MainApp;
+
+import javax.swing.JPanel;
+
+public class Panel_FinalDelivery extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_FinalDelivery() {
+		setBounds(186,11,662,546);
+	}
+
+}
