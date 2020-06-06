3d5f819a58d047e7e5b1796a1f07962778d2a1b3
mariannazch <65726276+mariannazch@users.noreply.github.com>
2020-06-06 13:49:16 +0300

    Update Panel_Backitup.java

--- Panel_Backitup.java
+++ Panel_Backitup.java
@@ -1,15 +1,17 @@
 package com.fixitup.MainApp;
-
+	
 import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class Panel_Backitup extends JPanel {
 
 	/**
-	 * Create the panel.
-	 */
+	* Create the panel.
+	*/
 	public Panel_Backitup() {
 		setBounds(186,11,662,546);
 
+		JButton btnNewButton = new JButton("New button");
+		add(btnNewButton);
 	}
-
 }
